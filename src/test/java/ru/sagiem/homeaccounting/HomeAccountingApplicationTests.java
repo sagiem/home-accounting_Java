8d5d101b8961389e1d2155236d76781f07948f3e
Maksim Kashin <sagiems@gmail.com>
2023-06-13 14:20:06 +0600

    add code

---
+++ src/test/java/ru/sagiem/homeaccounting/HomeAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sagiem.homeaccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
