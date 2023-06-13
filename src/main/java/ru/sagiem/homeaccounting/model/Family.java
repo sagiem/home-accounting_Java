d061de4d75c79c2f31bddfdd2d45eecd8a225811
Maksim Kashin <sagiems@gmail.com>
2023-06-13 16:59:52 +0600

    Добавил пользователей

---
+++ src/main/java/ru/sagiem/homeaccounting/model/Family.java
@@ -0,0 +1,24 @@
+package ru.sagiem.homeaccounting.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "families")
+public class Family {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "family")
+    private List<User> users;
+
+
+}
---
+++ src/main/java/ru/sagiem/homeaccounting/model/User.java
@@ -0,0 +1,28 @@
+package ru.sagiem.homeaccounting.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "family_id")
+    private Family family;
+}
