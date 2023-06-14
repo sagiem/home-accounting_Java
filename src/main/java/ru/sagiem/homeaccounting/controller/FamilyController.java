f38ca02236685814467de9cb5d9f6f6b18a4ecbe
Maksim Kashin <sagiems@gmail.com>
2023-06-14 17:27:27 +0600

    реализация пробного сервиса Family

--- src/main/java/ru/sagiem/homeaccounting/controller/FamilyController.java
+++ src/main/java/ru/sagiem/homeaccounting/controller/FamilyController.java
@@ -1,12 +1,15 @@
 package ru.sagiem.homeaccounting.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.sagiem.homeaccounting.model.Family;
 import ru.sagiem.homeaccounting.repository.FamilyRepository;
 import ru.sagiem.homeaccounting.repository.UserRepository;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +18,9 @@ public class FamilyController {
     final FamilyRepository familyRepository;
 
     @GetMapping("/family")
-    public ResponseEntity<Family> FamilyList(){
-        return familyRepository.findAll();
+    public ResponseEntity<Iterable<Family>> FamilyList(){
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(familyRepository.findAll());
     }
 }
