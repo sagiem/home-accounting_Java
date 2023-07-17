9b46365941d9c5a647cd64f668f0c6246bb45771
Maksim Kashin <sagiems@gmail.com>
2023-07-17 13:39:01 +0600

    исправил названия классов

--- src/main/java/ru/sagiem/whattobuy/WhatToBuyApplication.java
+++ src/main/java/ru/sagiem/whattobuy/WhatToBuyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HomeAccountingApplication {
+public class WhatToBuyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HomeAccountingApplication.class, args);
+		SpringApplication.run(WhatToBuyApplication.class, args);
 	}
 
 }
