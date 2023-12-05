5242f29cc22a93d436f7ce0e8f6d924859d7d4a2
Maksim Kashin <sagiems@gmail.com>
2023-12-05 17:18:49 +0600

    Задокументировал ShoppingProjectController

--- src/main/java/ru/sagiem/whattobuy/controller/rest/ShoppingProjectController.java
+++ src/main/java/ru/sagiem/whattobuy/controller/rest/ShoppingProjectController.java
@@ -30,7 +30,6 @@ public class ShoppingProjectController {
 
     private final ShoppingProjectService service;
 
-
     @Operation(
             summary = "Извлекает все проекты покупок для пользователя",
             description = "Извлечет результаты для всех Family group в которых находится пользователь, так и лично созданных проектов",
@@ -47,53 +46,129 @@ public class ShoppingProjectController {
         return ResponseEntity.ok(service.showAllUserCreatorOrFamilyGroup(userDetails));
     }
 
+    @Operation(
+            summary = "Добавляет проект покупок",
+            description = "Добавление проекта покупок либо в группу либо только для пользователя.",
+            tags = "get"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = ShoppingProjectDtoResponse.class)), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "403", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)})})
     @PostMapping("/add")
     public ResponseEntity<?> add(@RequestBody ShoppingProjectDtoRequest request,
                                  @AuthenticationPrincipal UserDetails userDetails) {
         return ResponseEntity.ok(service.add(request, userDetails));
     }
 
+    @Operation(
+            summary = "Поиск проекта покупок по id",
+            description = "Поиск проекта покупок по id",
+            tags = "get"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = ShoppingProjectDtoResponse.class)), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "403", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)})})
     @GetMapping("/search/{id}")
     public ResponseEntity<ShoppingProjectDtoResponse> search(@PathVariable("id") @Min(1) Integer id,
                                                              @AuthenticationPrincipal UserDetails userDetails) {
-
         return ResponseEntity.ok(service.searchId(id, userDetails));
     }
+
+    @Operation(
+            summary = "Обновление проекта покупок",
+            description = "Обновление проекта покупок, обновить может только создатель проекта",
+            tags = "get"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = ShoppingProjectDtoResponse.class)), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "403", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)})})
 
     @PatchMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") @Min(1) Integer id,
                                     ShoppingProjectDtoRequest request,
                                     @AuthenticationPrincipal UserDetails userDetails) {
         return ResponseEntity.ok(service.update(id, request, userDetails));
-
-
     }
 
+    @Operation(
+            summary = "Удалени проекта покупок",
+            description = "Удаление проекта покупо доступно только создателю проекта и если к проекту не привязаны покупки",
+            tags = "get"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = ShoppingProjectDtoResponse.class)), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "403", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)})})
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Boolean> delete(@PathVariable("id") @Min(1) Integer id, @AuthenticationPrincipal UserDetails userDetails) {
-
         return ResponseEntity.ok(service.delete(id, userDetails));
     }
 
+    @Operation(
+            summary = "Статистки проекта покупок",
+            description = "Показывает сколько покупок, привязанных к проекту, в работе, выполенно и не выполнено",
+            tags = "get"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = ShoppingProjectDtoResponse.class)), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "403", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)})})
     @GetMapping("/work_finish/{id}")
     public ResponseEntity<ShoppingProjectDtoWorkFinish> workFinishShoppingInProgect(@PathVariable("id") @Min(1) Integer id) {
         return ResponseEntity.ok(service.workFinishShoppingInProgect(id));
     }
 
+    @Operation(
+            summary = "Список покупок в работе",
+            description = "выводит список покупок, привязанных к проекту, которые находятся в работе",
+            tags = "get"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = ShoppingProjectDtoResponse.class)), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "403", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)})})
     @GetMapping("/work/{id}")
     public ResponseEntity<List<Shopping>> workShoppingInProgect(@PathVariable("id") @Min(1) Integer id) {
         return ResponseEntity.ok(service.workShoppingInProgect(id));
     }
 
+    @Operation(
+            summary = "Список выполненных покупок",
+            description = "выводит список покупок, привязанных к проекту, которые выполнены",
+            tags = "get"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = ShoppingProjectDtoResponse.class)), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "403", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)})})
     @GetMapping("/finish/{id}")
     public ResponseEntity<List<Shopping>> FinishShoppingInProgect(@PathVariable("id") @Min(1) Integer id) {
         return ResponseEntity.ok(service.FinishShoppingInProgect(id));
     }
 
+    @Operation(
+            summary = "Список не выполненных покупок",
+            description = "выводит список покупок, привязанных к проекту, которые не выполнены",
+            tags = "get"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(schema = @Schema(implementation = ShoppingProjectDtoResponse.class)), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "403", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema(implementation = ExceptionResponse.class), mediaType = APPLICATION_JSON_VALUE)})})
     @GetMapping("/not_work/{id}")
     public ResponseEntity<List<Shopping>> notWorkShoppingInProgect(@PathVariable("id") @Min(1) Integer id) {
         return ResponseEntity.ok(service.notWorkShoppingInProgect(id));
     }
-
 
 }
