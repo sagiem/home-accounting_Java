e77709a34f632cabdcc129eb68b3f00130765d7f
sagiem <sagiems@gmail.com>
2024-02-27 13:46:20 +0600

    Удалил FamilyGroupInvitationsController и FamilyGroupInvitationsService тк работа будет осуществляться через FamilyGroupController

--- src/main/java/ru/sagiem/whattobuy/service/FamilyGroupInvitationsService.java
+++
@@ -1,9 +0,0 @@
-package ru.sagiem.whattobuy.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class FamilyGroupInvitationsService {
-}
