64db4406d1d828980988bfc19bdd12ee9e0b1ffa
sagiem <sagiems@gmail.com>
2024-05-24 17:09:48 +0600

    Добавил интеграционные тесты для UserController

--- src/test/java/ru/sagiem/whattobuy/controller/UserControllerTest.java
+++ src/test/java/ru/sagiem/whattobuy/controller/UserControllerTest.java
@@ -2,16 +2,14 @@ package ru.sagiem.whattobuy.controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.engine.discovery.predicates.IsTestClassWithTests;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -34,17 +32,17 @@ class UserControllerIT {
         //given
         String email = "max@yandex.ru";
         var requestBuilder = MockMvcRequestBuilders.get("/api/v1/user/search/{email}", email);
-        // .with(jwt().jwt(builder -> builder))
+        //.with(jwt().jwt(builder -> builder.))
         //when
         this.mockMvc.perform(requestBuilder)
                 //then
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1));
+                .andExpect(jsonPath("$.id").isNumber());
     }
 
     @Test
-    @DisplayName("Отправляем email существующего пользователя и получаем в ответ его id")
+    @DisplayName("Отправляем email не существующего пользователя и получаем в ответ Exception")
     void searchUserByEmail_RequestIsNotValid_ThrowException() throws Exception {
 
         //given
---
+++ src/test/resources/resources/sql/users.sql
@@ -0,0 +1,2 @@
+insert into users (create_date_time, email, firstname, lastname, password)
+values (12-12-2024, 'test@test.ru', 'max', 'max', '12345678')
